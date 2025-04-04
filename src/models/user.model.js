6dd74f7b195453904fb950ee432eeb0be9acbb85
Yousif Abozid <yousif.abozid@yahoo.com>
2025-04-04 17:00:13 +0200

    Add password hashing and comparison methods to user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
 
 const userSchema = new mongoose.Schema(
 	{
@@ -14,12 +15,33 @@ const userSchema = new mongoose.Schema(
 			trim: true,
 			lowercase: true,
 			match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address']
+		},
+		password: {
+			type: String,
+			required: [true, 'Password is required'],
+			minlength: [6, 'Password must be at least 6 characters']
 		}
 	},
 	{
 		timestamps: true
 	}
 )
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+	if (!this.isModified('password')) {
+		return next()
+	}
+
+	const salt = await bcrypt.genSalt(10)
+	this.password = await bcrypt.hash(this.password, salt)
+	next()
+})
+
+// Method to compare passwords
+userSchema.methods.matchPassword = async function (enteredPassword) {
+	return await bcrypt.compare(enteredPassword, this.password)
+}
 
 const User = mongoose.model('User', userSchema)
 
