b69cf5df4765cb117f225f4bfb02b4ce166dd9f7
Yousif Abozid <yousif.abozid@yahoo.com>
2025-04-04 16:51:25 +0200

    Refactor user controller to use MongoDB for data operations instead of in-memory storage

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,85 +1,103 @@
-// Sample in-memory users database
-const users = [
-	{ id: '1', name: 'John Doe', email: 'john@example.com' },
-	{ id: '2', name: 'Jane Smith', email: 'jane@example.com' }
-]
+import User from '../models/user.model.js'
 
 /**
  * Get all users
  */
-export const getUsers = (req, res) => {
-	res.json({ users })
+export const getUsers = async (req, res, next) => {
+	try {
+		const users = await User.find({})
+		res.json({ users })
+	} catch (error) {
+		next(error)
+	}
 }
 
 /**
  * Get user by ID
  */
-export const getUserById = (req, res, next) => {
-	const { id } = req.params
-	const user = users.find((u) => u.id === id)
+export const getUserById = async (req, res, next) => {
+	try {
+		const { id } = req.params
+		const user = await User.findById(id)
 
-	if (!user) {
-		res.status(404)
-		return next(new Error('User not found'))
+		if (!user) {
+			res.status(404)
+			return next(new Error('User not found'))
+		}
+
+		res.json({ user })
+	} catch (error) {
+		next(error)
 	}
-
-	res.json({ user })
 }
 
 /**
  * Create a new user
  */
-export const createUser = (req, res, next) => {
-	const { name, email } = req.body
+export const createUser = async (req, res, next) => {
+	try {
+		const { name, email } = req.body
 
-	if (!name || !email) {
-		res.status(400)
-		return next(new Error('Name and email are required'))
+		if (!name || !email) {
+			res.status(400)
+			return next(new Error('Name and email are required'))
+		}
+
+		const userExists = await User.findOne({ email })
+
+		if (userExists) {
+			res.status(400)
+			return next(new Error('User with this email already exists'))
+		}
+
+		const newUser = await User.create({ name, email })
+		res.status(201).json({ user: newUser })
+	} catch (error) {
+		next(error)
 	}
-
-	const id = String(users.length + 1)
-	const newUser = { id, name, email }
-	users.push(newUser)
-
-	res.status(201).json({ user: newUser })
 }
 
 /**
  * Update user
  */
-export const updateUser = (req, res, next) => {
-	const { id } = req.params
-	const { name, email } = req.body
+export const updateUser = async (req, res, next) => {
+	try {
+		const { id } = req.params
+		const { name, email } = req.body
 
-	const userIndex = users.findIndex((u) => u.id === id)
+		const user = await User.findById(id)
 
-	if (userIndex === -1) {
-		res.status(404)
-		return next(new Error('User not found'))
+		if (!user) {
+			res.status(404)
+			return next(new Error('User not found'))
+		}
+
+		user.name = name || user.name
+		user.email = email || user.email
+
+		const updatedUser = await user.save()
+		res.json({ user: updatedUser })
+	} catch (error) {
+		next(error)
 	}
-
-	users[userIndex] = {
-		...users[userIndex],
-		...(name && { name }),
-		...(email && { email })
-	}
-
-	res.json({ user: users[userIndex] })
 }
 
 /**
  * Delete user
  */
-export const deleteUser = (req, res, next) => {
-	const { id } = req.params
-	const userIndex = users.findIndex((u) => u.id === id)
+export const deleteUser = async (req, res, next) => {
+	try {
+		const { id } = req.params
+		const user = await User.findById(id)
 
-	if (userIndex === -1) {
-		res.status(404)
-		return next(new Error('User not found'))
+		if (!user) {
+			res.status(404)
+			return next(new Error('User not found'))
+		}
+
+		await User.deleteOne({ _id: id })
+		res.json({ message: 'User deleted', user })
+	} catch (error) {
+		next(error)
 	}
-
-	const [deletedUser] = users.splice(userIndex, 1)
-
-	res.json({ message: 'User deleted', user: deletedUser })
 }
